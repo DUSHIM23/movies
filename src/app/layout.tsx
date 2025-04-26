23a24d0f7606ffc3ed28e8bfaaeed94e4bd8cca0
DUSHIME1212 <mdonavan33@gmail.com>
2025-04-26 14:49:43 +0200

    fix: correct casing of Suspense component in RootLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,7 +35,7 @@ export default function RootLayout({
         {children}
         <Footer/>
         </WatchlistProvider>
-            </suspense>
+            </Suspense>
         </body>
     </html>
   );
